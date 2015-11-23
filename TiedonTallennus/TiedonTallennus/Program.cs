6c64e7e81b887361b25a9b2dcea83d1338f7af2d
Satar Qaderzada <satar.qaderzada@stud.pointcollege.fi>
2015-11-23 15:12:34 +0200

    Toteutettu lämpötilan asettaminen ja tallennus.

---
+++ TiedonTallennus/TiedonTallennus/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiedonTallennus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Lämpötila = 0;
+            Console.Write("Anna uusi lämpötila: ");
+            string syöte = Console.ReadLine();
+            Lämpötila = int.Parse(syöte);
+
+            // tiedoston kirjoittaminen
+            string tiedosto = @"C:\Temp\Lämpötila.txt";
+            File.WriteAllText(tiedosto, Lämpötila.ToString());
+            Console.WriteLine("uusi lämpötila tallennettu.");
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
